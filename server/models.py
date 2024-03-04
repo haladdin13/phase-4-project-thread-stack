7a3b53ef66f6b6ffd91c0fdb56459395e4105c0c
nuburooj <nuburoojkhattak@gmail.com>
2024-03-03 19:39:05 -0500

    Made user Model in models.py

--- server/models.py
+++ server/models.py
@@ -4,3 +4,21 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.int, primary_key=True)
+    user_name = db.Column(db.str, unique=True)
+    password = db.Column(db.str)
+    tier = db.Column(db.int)
+    user_avatar = db.Column(db.str)
+    posts = db.Column(db.str)
+    region = db.Column(db.str)
+
+
+    def __repr__(self):
+        return f" User_name: {self.user_name} | Tier: {self.tier} | Avatar: {self.user_avatar} | Posts: {self.posts} | Region: {self.region}"
+
+
