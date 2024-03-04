a69cc958e1e8ec381252971598c519d88534a6cb
nuburooj <nuburoojkhattak@gmail.com>
2024-03-04 16:17:25 -0500

    Added relationships, serialize rules and validations to User and Thread models in models.py

--- server/models.py
+++ server/models.py
@@ -18,10 +18,49 @@ class User(db.Model, SerializerMixin):
     socials = db.Column(db.String)
 
     #RELATIONSHIPS
+    posts = db.relationship('Post', back_populates = 'user')
+    favorites = db.relationship('Favorite', back_populates = 'user')
+    categories = db.relationship('Category', back_populates = 'user')
 
     #SERIALIZATION RULES
+    serialize_rules = ('-posts.user', '-favorites.user', '-categories.user')
 
     #ADD VALIDATIONS
+
+    @validates('user_name')
+    def validate_user_name(self, key, user_name):
+        if isinstance(user_name, str) and len(user_name) > 5:
+            return user_name
+        else:
+            raise ValueError("Username must be longer than 5 characters")
+        
+    @validates('email')
+    def validate_email(self, key, email):
+        if isinstance(email, str) and len(email) > 5:
+            return email
+        else:
+            raise ValueError("Email must be longer than 5 characters")
+        
+    @validates('password')
+    def validate_password(self, key, password):
+        if isinstance(password, str) and len(password) > 5:
+            return password
+        else:
+            raise ValueError("Password must be longer than 5 characters")
+        
+    @validates('user_avatar')
+    def validate_user_avatar(self, key, user_avatar):
+        if isinstance(user_avatar, str):
+            return user_avatar
+        else:
+            raise ValueError("User avatar must be of type (.jpg)")
+        
+    @validates('socials')
+    def validate_socials(self, key, socials):
+        if isinstance(socials, str):
+            return socials
+        else:
+            raise ValueError("Please include Socials.")
 
     def __repr__(self):
         return f" User_name: {self.user_name} | Email: {self.email} | Tier: {self.tier} | Avatar: {self.user_avatar} | Socials: {self.socials}"
@@ -74,10 +113,29 @@ class Thread(db.Model, SerializerMixin):
         return f" Title: {self.title} | Content: {self.content} | Likes: {self.likes} | Created: {self.created_at} | Edited: {self.edited_at}"
 
     #RELATIONSHIPS
+    categories = db.relationship('Category', back_populates = 'thread')
+    favorites = db.relationship('Favorite', back_populates = 'thread')
+    posts = db.relationship('Post', back_populates = 'thread')
 
     #SERIALIZATION RULES
+    serialize_rules = ('-categories.thread', '-favorites.thread', '-posts.thread')
 
     #ADD VALIDATIONS
+    @validates('title')
+    def title(self, key, title):
+        if isinstance(title, str) and len(title) > 5:
+            return title
+        else:
+            raise ValueError('Title must be longer than 5 characters')
+        
+    @validates('content')
+    def content(self, key, content):
+        if isinstance(content, str) and len(content) > 5:
+            return content
+        else:
+            raise ValueError("Content must be greater than 5 characters")
+        
+    
 
 
 class Post(db.Model, SerializerMixin):
@@ -95,7 +153,7 @@ class Post(db.Model, SerializerMixin):
         return f" Content: {self.content} | User_ID: {self.user_id} | Thread_id: {self.thread_id} | likes: {self.likes} | Created: {self.created_at} | Edited: {self.edited_at}"
     
     #RELATIONSHIPS
-
+        
     #SERIALIZATION RULES
 
     #ADD VALIDATIONS
