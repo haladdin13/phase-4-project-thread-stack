7d715f97fccc014b23fa969e97608bed896a298e
nuburooj <nuburoojkhattak@gmail.com>
2024-03-03 19:56:57 -0500

    created Posts table in models.py

--- server/models.py
+++ server/models.py
@@ -9,16 +9,27 @@ from config import db
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
-    id = db.Column(db.int, primary_key=True)
-    user_name = db.Column(db.str, unique=True)
-    password = db.Column(db.str)
-    tier = db.Column(db.int)
-    user_avatar = db.Column(db.str)
-    posts = db.Column(db.str)
-    region = db.Column(db.str)
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    tier = db.Column(db.Integer)
+    user_avatar = db.Column(db.String)
+    posts = db.Column(db.String)
+    region = db.Column(db.String)
 
 
     def __repr__(self):
         return f" User_name: {self.user_name} | Tier: {self.tier} | Avatar: {self.user_avatar} | Posts: {self.posts} | Region: {self.region}"
 
 
+class Post(db.Model, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    user_id = db.Column(db.Integer)
+    thread_id = db.Column(db.Integer)
+    likes = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f" Content: {self.content} | User_ID: {self.user_id} | Thread_id: {self.thread_id} | likes: {self.likes}"
