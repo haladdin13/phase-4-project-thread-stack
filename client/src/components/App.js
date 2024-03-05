43f2df95595009dbbe41f3129d90f913bb27f986
Hussein Aladdin <haladdin13@outlook.com>
2024-03-05 12:15:47 -0500

    added components and imports to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
+
+import Dashboard from "./components/Dashboard";
+import Category from "./components/Category";
+import User from "./components/User";
+import GroupThread from "./components/GroupThread";
+import Thread from "./components/Thread";
+import Post from "./components/Post";
+import Favorite from "./components/Favorite";
+
+
+
 function App() {
-  return <h1>Project Client</h1>;
+
+  const [categories, setCategories] = useState([]);
+
+  <div>
+    <Route path = "/" element = {<Dashboard />} />
+    <Route path = "/categories" element = {<Category categories={categories}/>} />
+  </div>
 }
 
 export default App;
