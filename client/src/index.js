2d6367fae6e95ee109449319de6e630b65ab51f1
nuburooj <nuburoojkhattak@gmail.com>
2024-03-05 21:10:54 -0500

    made navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import './styles.css'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
