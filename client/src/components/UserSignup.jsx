20cf0ca20d54bbf11d74c658430222e841da37f3
nuburooj <nuburoojkhattak@gmail.com>
2024-05-04 16:27:16 -0400

    Added signup page, made new user from frontend and populated backend.

---
+++ client/src/components/UserSignup.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function UserSignup(){
+    const navigate = useNavigate()
+
+    const SignupTextInput = ({label, ...props}) => {
+
+        const [field, meta] = useField(props)
+        return(
+            <div className='form-group'>
+                <label htmlFor={props.id || props.name}>{label}</label>
+                <input className='text-input' {...field} {...props} />
+                {meta.touched && meta.error ? (
+                    <div className='error'>{meta.error}</div>
+                ) : null}
+            </div>
+        )
+    }
+
+    return(
+        <div> 
+            <Formik
+                initialValues={{
+                    user_name: "",
+                    email: "",
+                    password: ""
+                }}
+                validationSchema={Yup.object({
+                    user_name: Yup.string()
+                    .required('Username is required.'),
+                    email: Yup.string()
+                    .required('Email is required.'),
+                    password: Yup.string()
+                    .required('Password is required')
+                })}
+                onSubmit={(values, { setSubitting, resetForm }) => {
+
+                    fetch(`http://localhost:5555/signup`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(values)    
+                    })
+                    .then(res => res.json())
+                    .then(values => {
+                        console.log(values)
+                        navigate('/login')
+                    })
+                    .then( setSubitting(false), resetForm() );
+                }}
+                >
+                    <Form className='SubmitForm'>
+                        <SignupTextInput type="text" name="user_name" label="Username" />
+                        <SignupTextInput type="email" name="email" label="Email" />
+                        <SignupTextInput type="password" name="password" label="Password" />
+                        <button type="submit">Submit</button>
+                    </Form>
+            </Formik>
+        </div>
+    )
+
+}
+
+export default UserSignup;
