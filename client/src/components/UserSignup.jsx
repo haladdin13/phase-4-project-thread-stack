e3c8d0a12923ff8f26d0e2fb8fa4d8766d14bf89
nuburooj <nuburoojkhattak@gmail.com>
2024-05-05 11:30:05 -0400

    added styling to signup page

--- client/src/components/UserSignup.jsx
+++ client/src/components/UserSignup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import './SignupStyles.css';
 
 
 
@@ -23,45 +24,48 @@ function UserSignup(){
     }
 
     return(
-        <div> 
-            <Formik
-                initialValues={{
-                    user_name: "",
-                    email: "",
-                    password: ""
-                }}
-                validationSchema={Yup.object({
-                    user_name: Yup.string()
-                    .required('Username is required.'),
-                    email: Yup.string()
-                    .required('Email is required.'),
-                    password: Yup.string()
-                    .required('Password is required')
-                })}
-                onSubmit={(values, { setSubitting, resetForm }) => {
+        <div className='signup-heading'>
+            <h1>Signup</h1>
+            <div> 
+                <Formik
+                    initialValues={{
+                        user_name: "",
+                        email: "",
+                        password: ""
+                    }}
+                    validationSchema={Yup.object({
+                        user_name: Yup.string()
+                        .required('Username is required.'),
+                        email: Yup.string()
+                        .required('Email is required.'),
+                        password: Yup.string()
+                        .required('Password is required')
+                    })}
+                    onSubmit={(values, { setSubitting, resetForm }) => {
 
-                    fetch(`http://localhost:5555/signup`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(values)    
-                    })
-                    .then(res => res.json())
-                    .then(values => {
-                        console.log(values)
-                        navigate('/login')
-                    })
-                    .then( setSubitting(false), resetForm() );
-                }}
-                >
-                    <Form className='SubmitForm'>
-                        <SignupTextInput type="text" name="user_name" label="Username" />
-                        <SignupTextInput type="email" name="email" label="Email" />
-                        <SignupTextInput type="password" name="password" label="Password" />
-                        <button type="submit">Submit</button>
-                    </Form>
-            </Formik>
+                        fetch(`http://localhost:5555/signup`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(values)    
+                        })
+                        .then(res => res.json())
+                        .then(values => {
+                            console.log(values)
+                            navigate('/login')
+                        })
+                        .then( setSubitting(false), resetForm() );
+                    }}
+                    >
+                        <Form className='SubmitForm'>
+                            <SignupTextInput type="text" name="user_name" label="Username" />
+                            <SignupTextInput type="email" name="email" label="Email" />
+                            <SignupTextInput type="password" name="password" label="Password" />
+                            <button type="submit">Submit</button>
+                        </Form>
+                </Formik>
+            </div>
         </div>
     )
 
